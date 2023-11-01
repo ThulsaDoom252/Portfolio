d734aee3c8d7c60b3a8198bd7c369cd421ccfb17
ThulsaDoom252 <xenolm252@gmail.com>
2023-11-02 01:35:55 +0300

    Implemented greet component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'reset-css';
 import {ThemeProvider} from '@mui/material';
 import {theme} from "./muiStyles"
 import HeaderContainer from './components/Header/HeaderContainer';
+import Greet from './components/Greet';
 
 function App() {
 
     return (
         <ThemeProvider theme={theme}>
             <HeaderContainer/>
+            <Greet/>
         </ThemeProvider>
     );
 }
---
+++ src/components/Greet.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Greet = () => {
+
+    return (
+        <section className='greet-section'>
+            <div>
+                <span className='greet-label'>welcome to my world</span>
+                <h1 className='greet-title'>Hi, i'm Vladimir <br/> React developer </h1>
+                <h1 className='greet-based'>based in Ukraine</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Greet;
