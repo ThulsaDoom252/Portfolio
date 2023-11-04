d435790793057ea63e6428ab5d5b61dc1323aba6
ThulsaDoom252 <xenolm252@gmail.com>
2023-11-04 14:21:48 +0300

    Implemented Greet-section slide animation

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {delay} from '../common';
 
 const Greet = () => {
 
+    const [textIndex, setTextIndex] = useState(0)
+    const [slideAnimation, toggleSlideAnimation] = useState(false)
+    const texts = ['React developer', 'Ui/ux designer']
+
+    const handleAnimationSlide = async () => {
+        toggleSlideAnimation(true)
+        await delay(700)
+        setTextIndex((prevIndex) => (prevIndex === 0 ? 1 : 0))
+        toggleSlideAnimation(false)
+    }
+
+    useEffect(() => {
+        const timeout = 3500
+        const interval = setInterval(() => {
+            handleAnimationSlide()
+        }, timeout)
+
+        return () => clearInterval(interval)
+    }, [])
+
+
     return (
-        <section className='greet-section'>
-            <div>
-                <span className='greet-label'>welcome to my world</span>
-                <h1 className='greet-title'>Hi, i'm Vladimir <br/> React developer </h1>
-                <h1 className='greet-based'>based in Ukraine</h1>
+        <section className='greet'>
+            <div className='greet__main-text'>
+                <span className='greet__label'>welcome to my world</span>
+                <h1 className='greet__title'>Hi, i'm Vladimir <br/><h1
+                    className={`${slideAnimation && 'greet-animated-slide'} ${textIndex === 0 ? 'greet__react-text' : 'greet__designer-text'}`}>{texts[textIndex]}</h1>
+                </h1>
+                <h1 className='greet__subtitle'>based in Ukraine</h1>
             </div>
         </section>
     );
