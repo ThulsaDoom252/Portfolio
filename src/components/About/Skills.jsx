f78020390c4800f658e52d58c119369cfec271d7
ThulsaDoom252 <xenolm252@gmail.com>
2023-11-07 21:29:33 +0300

    Fixed text errors in skills section

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Skills = () => {
     const skills = [
-        {firstLabel: 'User experience design - Ux/Ux', secondLabel: 'Delight the user and make it work'},
-        {firstLabel: 'Web and user interface design - Development', secondLabel: 'Websites, web experiences'},
+        {firstLabel: 'User experience design - Ui/Ux', secondLabel: 'Delight the user and make it work'},
+        {firstLabel: 'Web and user interface design - development', secondLabel: 'Websites, web experiences'},
         {firstLabel: 'Front-end developer', secondLabel: 'Crafting perfection'},
         {firstLabel: 'Frameworks', secondLabel: 'React, NextJs'},
         {firstLabel: 'Languages', secondLabel: 'HTML, CSS, JS, TS'},
         {firstLabel: 'Preprocessors', secondLabel: 'Sass, Less'},
-        {firstLabel: 'State managers', secondLabel: 'Redux (Vanilla, Toolkit), Mobx, Context api'},
-        {firstLabel: 'Ui/Css frameworks', secondLabel: 'Bootstrap, Materials Ui, Ant-design, tailwind-css'},
+        {firstLabel: 'State managers', secondLabel: 'Redux (vanilla, toolkit), Mobx, Context api'},
+        {firstLabel: 'Ui/Css frameworks', secondLabel: 'Bootstrap, Materials Ui, Ant-design, Tailwind-css'},
     ]
     return (
         <ul>
